d1ac178b7275076ff151023fb833fa113e21052c
Suneeh <sonsey@web.de>
2023-12-02 15:29:32 +0100

    2.2

--- src/2.1/index.ts
+++ src/2.1/index.ts
@@ -1,11 +1,6 @@
 import { realData, testData } from "./testdata";
 
-function solve(
-  data: string[],
-  red: number,
-  green: number,
-  blue: number
-): number {
+function solve(data: string[]): number {
   let sumPossibleGames: number = 0;
   data.forEach((gameString, gameNr) => {
     let gameIsPossible: boolean = true;
@@ -15,9 +10,9 @@ function solve(
       values.forEach((value) => {
         let numberColor = value.split(" ");
         if (
-          (numberColor[1] === "blue" && +numberColor[0] > blue) ||
-          (numberColor[1] === "green" && +numberColor[0] > green) ||
-          (numberColor[1] === "red" && +numberColor[0] > red)
+          (numberColor[1] === "blue" && +numberColor[0] > 14) ||
+          (numberColor[1] === "green" && +numberColor[0] > 13) ||
+          (numberColor[1] === "red" && +numberColor[0] > 12)
         ) {
           gameIsPossible = false;
         }
@@ -30,5 +25,5 @@ function solve(
   return sumPossibleGames;
 }
 
-console.log(solve(testData, 12, 13, 14));
-console.log(solve(realData, 12, 13, 14));
+console.log(solve(testData));
+console.log(solve(realData));
---
+++ src/2.2/index.ts
@@ -0,0 +1,31 @@
+import { realData, testData } from "./testdata";
+
+function solve(data: string[]): number {
+  let sumPowerOfGames: number = 0;
+  data.forEach((gameString, gameNr) => {
+    let minRed: number = 0;
+    let minGreen: number = 0;
+    let minBlue: number = 0;
+    let sets: string[] = gameString.split(": ")[1].split("; ");
+    sets.forEach((setString) => {
+      let values: string[] = setString.split(", ");
+      values.forEach((value) => {
+        let numberColor = value.split(" ");
+        if (numberColor[1] === "red" && +numberColor[0] > minRed) {
+          minRed = +numberColor[0];
+        }
+        if (numberColor[1] === "green" && +numberColor[0] > minGreen) {
+          minGreen = +numberColor[0];
+        }
+        if (numberColor[1] === "blue" && +numberColor[0] > minBlue) {
+          minBlue = +numberColor[0];
+        }
+      });
+    });
+    sumPowerOfGames += minRed * minBlue * minGreen;
+  });
+  return sumPowerOfGames;
+}
+
+console.log(solve(testData));
+console.log(solve(realData));
